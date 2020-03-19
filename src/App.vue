6af5f9b94118821977d7f098dc6d96ee8fe6076f
anderson silva ribeiro <anderson_silvaribeiro@hotmail.com>
2020-03-19 20:32:22 -0300

    implementando formulário com bootstrap vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,39 @@
 <template>
 	<div id="app" class="container">
 		<h1>HTTP com Axios</h1>
+		<b-card>
+			<b-form-group label="Nome:">
+				<b-form-input type="text" size="lg"
+					v-model="usuario.nome" placeholder="Informe o Nome"></b-form-input>	
+			</b-form-group>
+			<b-form-group label="Email:">
+				<b-form-input type="email" size="lg"
+					v-model="usuario.email" placeholder="Informe o E-mail"></b-form-input>	
+			</b-form-group>
+			<hr>
+			<b-button @click.prevent="salvar"
+				size="lg" variant="primary">Salvar</b-button>
+		</b-card>
 	</div>
 </template>
 
 <script>
 export default {
-	created() {
-		this.$http.post('usuarios.json',  {
-			nome: 'teste1',
-			email: 'teste1@teste.com.br'
-		})
-		.then(res => console.log(res.data))
+	data() {
+		return {
+			usuario: {
+				nome: '',
+				email: ''
+			}
+		}
+	},
+	methods: {
+		salvar() {
+			console.log(this.usuario)
+		}
 	}
+
+	
 }
 </script>
 
