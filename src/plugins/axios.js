2a9d8bf4731c17bd60fab3271060612cb89591b1
anderson silva ribeiro <anderson_silvaribeiro@hotmail.com>
2020-03-19 21:32:00 -0300

    interceptação de resposta vuejs-axios-firebase

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -9,7 +9,7 @@ Vue.use({
             baseURL: process.env.VUE_APP_BASE_URL
         })
 
-        //intercepatando requisições
+        //intercepatando requisições, mudar métodos 
         Vue.prototype.$http.interceptors.request.use(config => {
            // eslint-disable-next-line no-console
            console.log(config.method)
@@ -18,7 +18,17 @@ Vue.use({
         //        config.method = 'put'
         //    }
            return config
-        })
+        }, error => Promise.reject(error))
+        //interceptando resposta, coloca id dentro e usuarios
+        Vue.prototype.$http.interceptors.response.use(response => {
+            // const array = []
+            // for(let chave in response.data) {
+            //     array.push({id: chave}, ...response.data[chave])
+            // }
+
+            // response.data = array
+            return response
+        }, error => Promise.reject(error))
     }
 
     
